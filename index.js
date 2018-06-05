4bf4c4147c6b7ef89d9a7c1a28488081c73c8340
kamalyadav11 <kyzx11@gmail.com>
2018-06-05 16:42:54 +0530

    pages added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+// import App from './App';
+import PageOne from './Pages/PageOne';
+//import PageTwo from './Pages/PageTwo';
+import createStackNavigator from 'react-navigation';
 
-AppRegistry.registerComponent('newProject', () => App);
+AppRegistry.registerComponent('newProject', () => PageOne);
